865a2757117046c8c93b1666a7e74f1219a1cdc8
Cristiano Almeida <cristiano.almeida@mercadolivre.com>
2023-10-13 04:30:21 -0300

    feat: AI Chat basic finalized

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,9 @@
+import { MessageInput } from '@hilla/react-components/MessageInput.js';
+import { MessageList, MessageListItem } from '@hilla/react-components/MessageList.js';
 import router from 'Frontend/routes.js';
+import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { ChatService } from './generated/endpoints';
 
 export default function App() {
   return <RouterProvider router={router} />;
